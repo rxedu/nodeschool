9fa5c7ffa666f0e9ea6948a4355d4544e922a9df
Evan Sosenko <razorx@evansosenko.com>
2015-11-09 12:42:20 -0800

    Complete remaining learnyounode lessons

---
+++ learnyounode/api.js
@@ -0,0 +1,31 @@
+var http = require('http');
+var url = require('url');
+
+function parsetime (time) {
+  return {
+    hour: time.getHours(),
+    minute: time.getMinutes(),
+    second: time.getSeconds()
+  };
+}
+
+function unixtime (time) {
+  return { unixtime : time.getTime() };
+}
+
+var server = http.createServer(function (req, res) {
+  var result;
+  var parsed = url.parse(req.url, true);
+  var time = new Date(parsed.query.iso);
+
+  if (/^\/api\/parsetime/.test(req.url)) {
+    result = parsetime(time);
+  } else if (/^\/api\/unixtime/.test(req.url)) {
+    result = unixtime(time);
+  }
+
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify(result));
+})
+
+server.listen(process.argv[2]);
---
+++ learnyounode/http-async.js
@@ -0,0 +1,27 @@
+var bl = require('bl');
+var http = require('http');
+
+var results = [];
+var count = 0
+
+function printResults () {
+  if (count === 3) {
+    for (var i=0; i < results.length; ++i) {
+      console.log(results[i]);
+    }
+  }
+}
+
+function getURL (index) {
+  http.get(process.argv[2 + index], function (response) {
+    response.pipe(bl(function (err, data) {
+      results[index] = data.toString();
+      count++;
+      printResults();
+    }));
+  });
+}
+
+for (var i=0; i < 3; ++i) {
+  getURL(i);
+}
