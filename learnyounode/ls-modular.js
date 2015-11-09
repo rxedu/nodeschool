b1539568947727d8b7077b12109fdaf7a7c7d081
Evan Sosenko <razorx@evansosenko.com>
2015-11-08 20:59:23 -0800

    Use forEach in some learnyounode lessons

--- learnyounode/ls-modular.js
+++ learnyounode/ls-modular.js
@@ -1,7 +1,5 @@
 var ls = require('./module-ls');
 
 ls(process.argv[2], process.argv[3], function (err, data) {
-  for (var i=0; i < data.length; ++i) {
-    console.log(data[i]);
-  }
+  data.forEach(function (f) { console.log(f); });
 });
